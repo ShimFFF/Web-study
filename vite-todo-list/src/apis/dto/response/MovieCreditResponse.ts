064726d48ccb62af9845c0488b7dda142dfcd015
shimfff <shimseawon1510@gmail.com>
2024-12-28 17:38:37 +0900

    :sparkles: feat: 영화 상세 조회 페이지

--- vite-todo-list/src/apis/dto/response/MovieCreditResponse.ts
+++ vite-todo-list/src/apis/dto/response/MovieCreditResponse.ts
@@ -1,58 +1,34 @@
 export interface MovieCreditsResponse {
+    id: number;
+    cast: CastMember[];
+    crew: CrewMember[];
+}
+
+export interface CastMember {
     adult: boolean;
-    backdrop_path: string;
-    belongs_to_collection: BelongsToCollection | null;
-    budget: number;
-    genres: Genre[];
-    homepage: string;
+    gender: number | null;
     id: number;
-    imdb_id: string;
-    origin_country: string[];
-    original_language: string;
-    original_title: string;
-    overview: string;
+    known_for_department: string;
+    name: string;
+    original_name: string;
     popularity: number;
-    poster_path: string;
-    production_companies: ProductionCompany[];
-    production_countries: ProductionCountry[];
-    release_date: string;
-    revenue: number;
-    runtime: number;
-    spoken_languages: SpokenLanguage[];
-    status: string;
-    tagline: string;
-    title: string;
-    video: boolean;
-    vote_average: number;
-    vote_count: number;
+    profile_path: string | null;
+    cast_id: number;
+    character: string;
+    credit_id: string;
+    order: number;
 }
 
-interface BelongsToCollection {
+export interface CrewMember {
+    adult: boolean;
+    gender: number | null;
     id: number;
+    known_for_department: string;
     name: string;
-    poster_path: string;
-    backdrop_path: string;
-}
-
-interface Genre {
-    id: number;
-    name: string;
-}
-
-interface ProductionCompany {
-    id: number;
-    logo_path: string;
-    name: string;
-    origin_country: string;
-}
-
-interface ProductionCountry {
-    iso_3166_1: string;
-    name: string;
-}
-
-interface SpokenLanguage {
-    english_name: string;
-    iso_639_1: string;
-    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string | null;
+    credit_id: string;
+    department: string;
+    job: string;
 }
--- vite-todo-list/src/pages/movie/MovieDetail.tsx
+++ vite-todo-list/src/pages/movie/MovieDetail.tsx
@@ -4,7 +4,7 @@ const MovieDetail = () => {
 
   return (
     <div>
-      <h1>영화 상세 페이지</h1>
+      <MovieDetail/>
     </div>
   );
 }
